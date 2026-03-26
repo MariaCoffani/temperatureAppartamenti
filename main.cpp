73a5b04a6a5e38f56c8df81e31d478929e4b71c9
Maria Coffani <coffani_maria@yahoo.it>
2026-03-26 11:47:59 +0100

    preparazione esercizio guidato

--- main.cpp
+++ main.cpp
@@ -2,41 +2,53 @@
 #include "temperature.h"
 using namespace std;
 
+
+/*GUARDA LA STRUTTURA, IL DO WHILE MI SERVE PER POTER RIPROPORRE IL MENU DOPO OGNI SCELTA.
+
+ 1. Aggiungi le voci mancanti nel menu:
+ 2. Visualizzare tabella
+ 3. Totale appartamento
+ 0. Esci
+
+2. Nei case:
+- case 2: richiama la funzione mostraTabella(m)
+- case 3: richiama la funzione totaleAppartamento(m)
+
+4. Il menu deve continuare a essere mostrato finché l’utente non sceglie 0
+
+Domanda:
+Se l’utente seleziona l’opzione 2 o 3 prima di aver inserito i dati, il programma utilizza valori non
+inizializzati presenti in memoria.
+Modifica il codice per evitare questo comportamento e garantire che le operazioni vengano eseguite
+solo su dati validi.
+
+*/
+
+
 int main() {
     float m[APP][GIORNI];
     int scelta;
-    bool datiInseriti = false;   // stato del programma
-    //progetto sviluppo
-    //commento da annullare
 
     do {
         cout << "\nMENU\n";
         cout << "1. Inserire consumi\n";
-        cout << "2. Visualizzare tabella\n";
-        cout << "3. Totale appartamento\n";
-        cout << "0. Esci\n";
-
         cin >> scelta;
 
         switch (scelta) {
 
             case 1:
                 inserisciConsumi(m);
-                datiInseriti = true;   // ora i dati esistono
                 break;
 
             case 2:
-                if (!datiInseriti)
-                    cout << "Inserire prima i dati\n";
-                else
-                    mostraTabella(m); //m=usa quella matrice per lavorare sui dati
+
+
                 break;
 
             case 3:
-                if (!datiInseriti)
-                    cout << "Inserire prima i dati\n";
-                else
-                    totaleAppartamento(m);
+
+
+
                 break;
 
             case 0:
--- temperature.cpp
+++ temperature.cpp
@@ -2,39 +2,32 @@
 #include "temperature.h"
 using namespace std;
 
+
+//FUNZIONE VISTA INSIEME IN LAB
 void inserisciConsumi(float m[APP][GIORNI]) {
     for (int i = 0; i < APP; i++) {
         for (int j = 0; j < GIORNI; j++) {
-            do {
                 cout << "Consumo appartamento " << i << " giorno " << j << ": ";
                 cin >> m[i][j];
-            } while (m[i][j] < 0);
-            //Il ciclo continua a chiedere il dato finché il valore inserito non è valido.
-            //In questo caso, il valore è valido solo se è maggiore o uguale a zero.
-          //  appartamento i
-         //   giorno j
+            }
         }
     }
+
+
+//IMPLEMENTA LE FUNZIONI RICHIESTA
+
+//Implementa la funzione per poter stampare i dati inseriti
+void mostraTabella(float m[APP][GIORNI]) {
+
 }
 
-void mostraTabella(float m[APP][GIORNI]) {
-    for (int i = 0; i < APP; i++) {
-        for (int j = 0; j < GIORNI; j++) {
-            cout << m[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+
+/*La funzione deve chiedere all’utente quale appartamento analizzare
+Deve controllare che il valore inserito sia valido
+Deve calcolare la somma dei consumi di quell’appartamento
+Deve stampare il totale
+*/
 
 void totaleAppartamento(float m[APP][GIORNI]) {
-    int a;
-    cout << "Inserisci appartamento: ";
-    cin >> a;
-
-    float somma = 0;
-    for (int j = 0; j < GIORNI; j++) {
-        somma += m[a][j];
-    }
-
-    cout << "Totale: " << somma << endl;
 }
+
