f429acfbcbe0e146fd4589d12093abd6822de194
alessandropignoli <alessandro.pignoli@icloud.com>
2026-04-05 21:47:52 +0200

    progetto temperature completato

--- temperature.cpp
+++ temperature.cpp
@@ -2,32 +2,52 @@
 #include "temperature.h"
 using namespace std;
 
-
-//FUNZIONE VISTA INSIEME IN LAB
+// FUNZIONE VISTA INSIEME IN LAB
 void inserisciConsumi(float m[APP][GIORNI]) {
     for (int i = 0; i < APP; i++) {
         for (int j = 0; j < GIORNI; j++) {
-                cout << "Consumo appartamento " << i << " giorno " << j << ": ";
-                cin >> m[i][j];
-            }
+            cout << "Consumo appartamento " << i << " giorno " << j << ": ";
+            cin >> m[i][j];
         }
     }
+}
 
-
-//IMPLEMENTA LE FUNZIONI RICHIESTA
-
-//Implementa la funzione per poter stampare i dati inseriti
+// Stampa tutta la matrice dei consumi
 void mostraTabella(float m[APP][GIORNI]) {
+    cout << "\nTABELLA CONSUMI\n";
 
+    for (int i = 0; i < APP; i++) {
+        cout << "Appartamento " << i << ": ";
+        for (int j = 0; j < GIORNI; j++) {
+            cout << m[i][j] << "\t";
+        }
+        cout << endl;
+    }
 }
 
-
-/*La funzione deve chiedere all’utente quale appartamento analizzare
-Deve controllare che il valore inserito sia valido
-Deve calcolare la somma dei consumi di quell’appartamento
-Deve stampare il totale
+/*
+La funzione deve:
+- chiedere all’utente quale appartamento analizzare
+- controllare che il valore inserito sia valido
+- calcolare la somma dei consumi di quell’appartamento
+- stampare il totale
 */
-
 void totaleAppartamento(float m[APP][GIORNI]) {
-}
+    int appartamento;
+    float totale = 0;
 
+    cout << "Inserisci il numero dell'appartamento (0-" << APP - 1 << "): ";
+    cin >> appartamento;
+
+    if (appartamento < 0 || appartamento >= APP) {
+        cout << "Appartamento non valido\n";
+        return;
+    }
+
+    for (int j = 0; j < GIORNI; j++) {
+        totale += m[appartamento][j];
+    }
+
+    cout << "Totale consumi appartamento " << appartamento << ": "
+         << totale << " kWh\n";
+}
--- temperature.h
+++ temperature.h
@@ -4,11 +4,8 @@
 const int APP = 3;
 const int GIORNI = 7;
 
-
-
-//PROSEGUI DICHIARANDO LE ALTRE DUE FUNZIONI NECESSARIE
 void inserisciConsumi(float m[APP][GIORNI]);
-
-
+void mostraTabella(float m[APP][GIORNI]);
+void totaleAppartamento(float m[APP][GIORNI]);
 
 #endif
