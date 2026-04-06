daf550c06216bf360a9f01c0c46a349a749f9f9f
Kareissa <kareissa20@gmail.com>
2026-04-06 12:18:39 +0200

    progetto completato

--- temperature.cpp
+++ temperature.cpp
@@ -2,32 +2,40 @@
 #include "temperature.h"
 using namespace std;
 
-
-//FUNZIONE VISTA INSIEME IN LAB
 void inserisciConsumi(float m[APP][GIORNI]) {
     for (int i = 0; i < APP; i++) {
-        for (int j = 0; j < GIORNI; j++) {
-                cout << "Consumo appartamento " << i << " giorno " << j << ": ";
-                cin >> m[i][j];
-            }
+        for (int i = 0; i < GIORNI; i++) {
+            cout << "Consumo appartamento " << i << " giorno " << i << ": " << endl;
+            cin >> m[i][i];
         }
     }
-
-
-//IMPLEMENTA LE FUNZIONI RICHIESTA
-
-//Implementa la funzione per poter stampare i dati inseriti
-void mostraTabella(float m[APP][GIORNI]) {
-
 }
 
-
-/*La funzione deve chiedere all’utente quale appartamento analizzare
-Deve controllare che il valore inserito sia valido
-Deve calcolare la somma dei consumi di quell’appartamento
-Deve stampare il totale
-*/
+void mostraTabella(float m[APP][GIORNI]) {
+    for (int i = 0; i < APP; i++) {
+        for (int j = 0; j < GIORNI; j++) {
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 void totaleAppartamento(float m[APP][GIORNI]) {
-}
 
+    int app;
+    cout << "Numero appartamento: " << endl;
+    cin >> app;
+
+    if (app < 0 || app >= APP) {
+        cout << "Numero non valido" << endl;
+        return;
+    }
+
+    float totale = 0;
+
+    for (int j = 0; j < GIORNI; j++) {
+        totale = totale + m[app][j];
+    }
+
+    cout << "Totale consumo: " << totale << endl;
+}
