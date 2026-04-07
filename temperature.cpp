cd5071fec316dc03141d786fe560fc55569670f9
Gabriele <gabrielemaninetti1@gmail.com>
2026-04-07 16:02:53 +0200

    compito completato

--- temperature.cpp
+++ temperature.cpp
@@ -7,18 +7,24 @@ using namespace std;
 void inserisciConsumi(float m[APP][GIORNI]) {
     for (int i = 0; i < APP; i++) {
         for (int j = 0; j < GIORNI; j++) {
-                cout << "Consumo appartamento " << i << " giorno " << j << ": ";
-                cin >> m[i][j];
-            }
+            cout << "Consumo appartamento " << i << " giorno " << j << ": ";
+            cin >> m[i][j];
         }
     }
+}
 
 
 //IMPLEMENTA LE FUNZIONI RICHIESTA
 
 //Implementa la funzione per poter stampare i dati inseriti
 void mostraTabella(float m[APP][GIORNI]) {
-
+    for (int i = 0; i < APP; i++) {
+        cout << "Appartamento " << i << ": ";
+        for (int j = 0; j < GIORNI; j++) {
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
 
@@ -29,5 +35,18 @@ Deve stampare il totale
 */
 
 void totaleAppartamento(float m[APP][GIORNI]) {
+    int sceltaApp;
+    cout << "Di quale appartamento (0-2) vuoi il totale? ";
+    cin >> sceltaApp;
+
+    if (sceltaApp >= 0 && sceltaApp < APP) {
+        float somma = 0;
+        for (int j = 0; j < GIORNI; j++) {
+            somma += m[sceltaApp][j];
+        }
+        cout << "Totale consumi: " << somma << " kWh" << endl;
+    } else {
+        cout << "Indice non valido!" << endl;
+    }
 }
 
